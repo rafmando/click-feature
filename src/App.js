db681f7677dfc01fff32ba94ee52a67cbd2b7fab
rafmando <rafmando1111@gmail.com>
2022-05-15 11:04:37 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react'
+
+const data = [
+  { name: 'Item A', price: 125 },
+  { name: 'Item B', price: 230 },
+  { name: 'Item C', price: 295 },
+  { name: 'Item D', price: 245 },
+  { name: 'Item E', price: 900 },
+  { name: 'Item F', price: 875 },
+  { name: 'Item G', price: 235 },
+  { name: 'Item H', price: 400 },
+];
 
 function App() {
+
+  const lowToHighProds = data.sort((a,b) => {
+    if(a.price > b.price){
+      return 1
+    } else {
+      return -1
+    }
+  }).map((prod) => {
+    return prod
+  })
+
+  const firstFiveProds = lowToHighProds.slice(0,5)
+  const [products,setProducts] = useState(firstFiveProds)
+  const [buttonText,setButtonText] = useState('View All')
+  const newButtonText = buttonText === 'View All' ? 'Collapse' : 'View All'
+
+  const handleClick = () => {
+    setButtonText(newButtonText)
+
+    if(buttonText === 'Collapse') {
+      setProducts(firstFiveProds)
+    } else if(buttonText === 'View All') {
+      setProducts(lowToHighProds)
+    }
+    
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div className="container">
+   <button onClick={handleClick}>{buttonText}</button>
+   <ul aria-label="products-list">
+     {products.map((prod) => {
+       return (
+         <li key={prod.name}>
+           <p>{prod.name}</p>
+           <p>£{prod.price}</p>
+         </li>
+       )
+     })}
+   </ul>
+   </div>
   );
 }
 
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,49 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('Button unclicked', () => {
+  it('renders button correctly', () => {
+    render(<App />);
+
+    const button = screen.getByRole('button', {name: 'View All'})
+    expect(button.textContent).toBe('View All')
+  });
+  
+  it('renders data correctly', () => {
+    render(<App />)
+
+    const productList = screen.getByRole('list', {
+      name: /products/i,
+    })
+  
+    expect(productList).toMatchSnapshot()
+  })
+})
+
+describe('Button clicked', () => {
+  it('renders button correctly when clicked', () => {
+    render(<App/>);
+
+    const button = screen.getByRole('button', {name: 'View All'})
+    expect(button.textContent).toBe('View All')
+
+    fireEvent.click(button)
+    expect(button.textContent).toBe('Collapse')
+  });
+  
+  it('renders data correctly when clicked', () => {
+    render(<App />)
+
+    const productList = screen.getByRole('list', {
+      name: /products/i,
+    })
+
+    const button = screen.getByRole('button', {name: 'View All'})
+    fireEvent.click(button)
+    expect(productList).toMatchSnapshot()
+  })
+})
+
+
+
+
